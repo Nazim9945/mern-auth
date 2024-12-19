33171dad57f2f58b7f8dc34cdb92f448757528df
Nazim Saifi <saifinazim2311@gmail.com>
2024-12-19 21:43:54 +0530

    added signup signin controller

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, RequestHandler, Response } from "express";
-
-
+import User from "../models/user";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 //signup
 
@@ -9,9 +10,35 @@ const signup: RequestHandler = async (
   res: Response
 ) => {
   try {
-    res.status(200).json({
-      message: "signup"
+    const {firstName,lastName="",email,password}=req.body;
+    if(!firstName  || !email || !password){
+        res.status(404).json({
+            messahe:"All fields are required"
+        })
+        return;
+    }
+    const isExist=await User.findOne({email});
+    if(isExist){
+        res.status(411).json({
+            message:"This user has an account"
+        })
+        return;
+    }
+    const hashpwd=await bcrypt.hash(password,10);
+
+    const newuser=await User.create({firstName,lastName,email,password:hashpwd});
+    const payload={
+        id:newuser._id,        
+    }
+    const token=jwt.sign(payload,process.env.JWT_SECRET || "",{
+        expiresIn:'1d'
     });
+
+    res.status(200).json({
+        message:"user created !!",
+        token
+    })
+    return;
   } catch (error) {
     console.log(error);
     res.status(404).json({
@@ -24,7 +51,40 @@ const signup: RequestHandler = async (
 //signin
 const signin:RequestHandler=async(req:Request,res:Response)=>{
         try {
-            
+            const {email,password}=req.body;
+            if(!email || !password){
+                res.status(404).json(
+                    {
+                        message:"All fields are required"
+                    }
+                )
+            }
+            const isExist=await User.findOne({email})
+            if(!isExist){
+                res.status(404).json({
+                    message:"please register first!"
+                })
+                return;
+            }
+          if(await bcrypt.compare(isExist.password,password)){
+                const payload = {
+                  id: isExist._id,
+                };
+                const token = jwt.sign(payload, process.env.JWT_SECRET || "", {
+                  expiresIn: "1d",
+                });
+
+                res.status(200).json({
+                  message: "user logged in successfully!!",
+                  token,
+                });
+                return;
+          }
+          res.status(404).json({
+            message:"password is incorrect";
+          })
+          return;
+
         } catch (error) {
             console.log(error);
             res.json(404).json({
